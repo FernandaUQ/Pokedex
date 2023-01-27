f2f35ae7af894b3fac2ac27b890d37b111d2c1da
Fernanda <fernanda.uchoa@rocketwave.tech>
2023-01-27 18:37:34 -0300

    removing buffer issue

--- src/main/java/com/project/pokedex/pokemon/infrastructure/accessdata/PokemonWebClientQueryRepository.java
+++ src/main/java/com/project/pokedex/pokemon/infrastructure/accessdata/PokemonWebClientQueryRepository.java
@@ -5,16 +5,26 @@ import com.project.pokedex.pokemon.core.context.PokemonListing;
 import com.project.pokedex.pokemon.core.read.Pokemon;
 import com.project.pokedex.util.classes.Page;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
-public class PokemonWebClientQueryRepository implements PokemonQueryRepository{
+public class PokemonWebClientQueryRepository implements PokemonQueryRepository {
 
     private final WebClient webClient;
 
     public PokemonWebClientQueryRepository() {
-        this.webClient = WebClient.create("https://pokeapi.co/api/v2");
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+        this.webClient = WebClient.builder()
+                .baseUrl("https://pokeapi.co/api/v2")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(strategies)
+                .build();
     }
 
 
@@ -41,7 +51,8 @@ public class PokemonWebClientQueryRepository implements PokemonQueryRepository{
                 .uri("/pokemon?offset={offset}&limit={limit}", offset, limit)
                 .accept(MediaType.ALL)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Page<PokemonConsumerListing>>() {})
+                .bodyToMono(new ParameterizedTypeReference<Page<PokemonConsumerListing>>() {
+                })
                 .block();
     }
 }
